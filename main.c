d120e3ad56f37ce4988cb1fb20531b481675b619
9ziggy9 <davidarogers@protonmail.com>
2023-03-10 09:25:12 -0800

    Move encoding complete

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include <stdbool.h>
 
 typedef enum {
-  BLACK_RESIGN = 0, WHITE_RESIGN,
-  BLACK_PAWN, BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP,
-  BLACK_QUEEN, BLACK_KING,
-  WHITE_PAWN, WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP,
-  WHITE_QUEEN, WHITE_KING
-} Piece;
+  BLACK_RESIGN  = 0x45, WHITE_RESIGN  = 0x65,
+  BLACK_PAWN	= 0x50, BLACK_ROOK    = 0x52,
+  BLACK_KNIGHT	= 0x4E, BLACK_BISHOP  = 0x42,
+  BLACK_QUEEN	= 0x51, BLACK_KING    = 0x4B,
+  WHITE_PAWN	= 0x70, WHITE_ROOK    = 0x72,
+  WHITE_KNIGHT	= 0x6E, WHITE_BISHOP  = 0x62,
+  WHITE_QUEEN	= 0x71, WHITE_KING    = 0x6B
+} PieceEncoding;
 
 typedef enum {
   // 0x88 encoding scheme
@@ -20,20 +22,38 @@ typedef enum {
   A6 = 0x50, B6, C6, D6, E6, F6, G6, H6,
   A7 = 0x60, B7, C7, D7, E7, F7, G7, H7,
   A8 = 0x70, B8, C8, D8, E8, F8, G8, H8
-} Square;
+} SquareEncoding;
 
-char to0x88(const char *square) {
-  char rank = square[0] - 'a';
-  char file = square[1] - '1';
-  return (char)(file << 4) + rank;
+typedef struct MoveEncoding {
+  PieceEncoding piece;
+  SquareEncoding square;
+} MoveEncoding;
+
+typedef MoveEncoding GameEncoding[255];
+
+SquareEncoding to0x88(const char rk, const char fl) {
+  SquareEncoding rank = rk - 'a';
+  SquareEncoding file = fl - '1';
+  return (file << 4) + rank;
 }
+
+PieceEncoding toPieceByte(const char piece) { return (PieceEncoding) piece; }
+
+MoveEncoding encodeMove(const char *input) {
+  MoveEncoding m = {
+    .piece = toPieceByte(input[0]),
+    .square = to0x88(input[1], input[2])
+  };
+  return m;
+}
+
+void appendMove(GameEncoding *g, MoveEncoding m) {}
 
 int main(void) {
   char input[3];
   printf("> ");
   scanf("%3[^\n]s", input);
-  printf("%x\n", to0x88(input));
-  printf("%d\n", to0x88(input) == E4);
+  printf("%x\n", input[0]);
   printf("\n");
   return 0;
 }
